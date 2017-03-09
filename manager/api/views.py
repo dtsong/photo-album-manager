e71eabc4d7ff780d1e92b919cdedc1c24a65e656
Daniel Song <xdtsong@gmail.com>
2017-03-09 15:58:01 -0800

    Refactor Album's views to use APIView in views.py

--- manager/api/views.py
+++ manager/api/views.py
@@ -1,9 +1,12 @@
 from .models import Photo, Album
 from .serializers import PhotoSerializer, AlbumSerializer
+from django.http import Http404
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+from rest_framework import status
 
 
 @api_view(['GET'])
@@ -24,11 +27,48 @@ class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PhotoSerializer
 
 
-class AlbumList(generics.ListCreateAPIView):
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
+class AlbumList(APIView):
+    """
+    List all albums, or create a new album.
+    """
+    def get(self, request, format=None):
+        albums = Album.objects.all()
+        serializer = AlbumSerializer(albums, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = AlbumSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AlbumDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Album.objects.all()
-    serializer_class = AlbumSerializer
+
+class AlbumDetail(APIView):
+    """
+    Retrieve, update, or delete an album instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Album.objects.get(pk=pk)
+        except Album.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        album = self.get_object(pk)
+        serializer = AlbumSerializer(album)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        album = self.get_object(pk)
+        serializer = AlbumSerializer(album, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        album = self.get_object(pk)
+        album.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
