0c0766d34af6729ed110149331a852d0ac790d6e
Daniel Song <xdtsong@gmail.com>
2017-03-08 23:13:44 -0800

    Refactor views.py to use API view wrappers

--- manager/api/views.py
+++ manager/api/views.py
@@ -1,21 +1,11 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Photo
 from .serializers import PhotoSerializer
 
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
 
-
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def photo_list(request):
     """
     List all photos, or create a new photo.
@@ -23,39 +13,37 @@ def photo_list(request):
     if request.method == 'GET':
         photos = Photo.objects.all()
         serializer = PhotoSerializer(photos, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = PhotoSerializer(data=data)
+        serializer = PhotoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def photo_detail(request, pk):
     """
-    Retrieve, update or delete a photo.
+    Retrieve, update or delete a photo instance.
     """
     try:
         photo = Photo.objects.get(pk=pk)
     except Photo.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = PhotoSerializer(photo)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PhotoSerializer(photo, data=data)
+        serializer = PhotoSerializer(photo, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         photo.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
