9c9cfeedf931a9414988f286ba2a94207c42cd35
Daniel Song <xdtsong@gmail.com>
2017-03-09 00:18:55 -0800

    Add endpoint for API root and configure URLs for API view and Albums

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^photos/$', views.PhotoList.as_view()),
     url(r'^photos/(?P<pk>[0-9]+)/$', views.PhotoDetail.as_view()),
+    url(r'^albums/$', views.AlbumList.as_view()),
+    url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view()),
+    url(r'^$', views.api_root),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- manager/api/views.py
+++ manager/api/views.py
@@ -1,6 +1,17 @@
 from .models import Photo, Album
 from .serializers import PhotoSerializer, AlbumSerializer
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'albums': reverse('album-list', request=request, format=format),
+        'views': reverse('photo-list', request=request, format=format)
+    })
 
 
 class PhotoList(generics.ListCreateAPIView):
