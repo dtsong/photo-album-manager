c3034d74b26efa3094d6a2b2b54a0d84486897f5
Daniel Song <xdtsong@gmail.com>
2017-03-09 00:24:14 -0800

    Configure routes to be given names

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
+# API Endpoints
 urlpatterns = [
-    url(r'^photos/$', views.PhotoList.as_view()),
-    url(r'^photos/(?P<pk>[0-9]+)/$', views.PhotoDetail.as_view()),
-    url(r'^albums/$', views.AlbumList.as_view()),
-    url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view()),
     url(r'^$', views.api_root),
+    url(r'^photos/$', views.PhotoList.as_view(), name='photo-list'),
+    url(r'^photos/(?P<pk>[0-9]+)/$', views.PhotoDetail.as_view(), name='photo-detail'),
+    url(r'^albums/$', views.AlbumList.as_view(), name='album-list'),
+    url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view(), name='album-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
