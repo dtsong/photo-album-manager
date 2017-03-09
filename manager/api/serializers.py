99d217215174cfe4bd6b837c898c9e64e7ca9ac1
Daniel Song <xdtsong@gmail.com>
2017-03-09 00:10:14 -0800

    Add serializer for Album

--- manager/api/serializers.py
+++ manager/api/serializers.py
@@ -1,5 +1,13 @@
 from rest_framework import serializers
-from .models import Photo
+from .models import Photo, Album
+
+
+class AlbumSerializer(serializers.ModelSerializer):
+    photos = PhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Album
+        fields = ('id', 'title', 'photos')
 
 
 class PhotoSerializer(serializers.ModelSerializer):
