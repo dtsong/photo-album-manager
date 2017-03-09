a5f14e836de97a863166eeee16358dca351e9636
Daniel Song <xdtsong@gmail.com>
2017-03-09 00:01:05 -0800

    Refactor views.py to use generic views and add records using UI

--- manager/api/views.py
+++ manager/api/views.py
@@ -1,50 +1,13 @@
 from .models import Photo
 from .serializers import PhotoSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 
-
-class PhotoList(APIView):
-    """
-    List all photos, or create a new photo.
-    """
-    def get(self, request, format=None):
-        photos = Photo.objects.all()
-        serializer = PhotoSerializer(photos, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PhotoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PhotoList(generics.ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
 
 
-class PhotoDetail(APIView):
-    """
-    Retrieve, update or delete a photo instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Photo.objects.get(pk=pk)
-        except Photo.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        photo = self.get_object(pk)
-        serializer = PhotoSerializer(photo)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        photo = self.get_object(pk)
-        serializer = PhotoSerializer(photo)
-        return Response(serializer.data)
-
-    def delete(self, request, pk, format=None):
-        photo = self.get_object(pk)
-        photo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
