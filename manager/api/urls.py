df59e2cff2b0e0d3269c184a00b3d33b7280422e
Daniel Song <xdtsong@gmail.com>
2017-03-10 14:14:09 -0800

    Rerun migrations with User and add api_root url to urls.py

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 # API Endpoints
 urlpatterns = [
+    url(r'^', views.api_root),
     url(r'^albums/$', views.AlbumList, name='album-list'),
     url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view()),
-    url(r'^photos/$', views.PhotoList, name='photos-list'),
+    url(r'^photos/$', views.PhotoList, name='photo-list'),
     url(r'^photos/(?P<pk>[0-9]+)/$', views.PhotoDetail.as_view()),
     url(r'^users/$', views.UserList, name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
