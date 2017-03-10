780d1c54f2f8f823b6a5800da29ec5a1e60479f3
Daniel Song <xdtsong@gmail.com>
2017-03-10 03:58:09 -0800

    URLs seem to be tripping HyperlinkedRelatedFields when models require two URLFields.

--- manager/api/models.py
+++ manager/api/models.py
@@ -4,21 +4,9 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ['title']
-
 
 class Photo(models.Model):
-    albumId = models.ForeignKey(Album, related_name='photos', on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, related_name='photos', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    photoUrl = models.URLField()
+    url = models.URLField()
     thumbnailUrl = models.URLField()
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        ordering = ['title']
--- manager/api/serializers.py
+++ manager/api/serializers.py
@@ -6,12 +6,19 @@ class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
-        fields = ('albumId', 'id', 'title', 'photoUrl', 'thumbnailUrl')
+        fields = ('title', 'url', 'thumbnailUrl')
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    photos = PhotoSerializer(many=True, source='photo_set', read_only=True)
+    photos = PhotoSerializer(many=True)
 
     class Meta:
         model = Album
         fields = ('title', 'photos')
+
+    def create(self, validated_data):
+        photos_data = validated_data.pop('photos')
+        album = Album.objects.create(**validated_data)
+        for photo_data in photos_data:
+            Photo.objects.create(album=album, **photo_data)
+        return album
