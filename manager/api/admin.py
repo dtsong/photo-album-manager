e88f37c0c88c4c78e0b6424fb16862b51a5f3251
Daniel Song <xdtsong@gmail.com>
2017-03-10 15:35:16 -0800

    Enable Django Admin site and register Album and Photo models

--- manager/api/admin.py
+++ manager/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Album, Photo
 
 # Register your models here.
+admin.site.register(Album)
+admin.site.register(Photo)
--- manager/manager/urls.py
+++ manager/manager/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('api.urls')),
+    url(r'^admin/', admin.site.urls)
 ]
