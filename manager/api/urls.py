69d33bc2d3895a5b977e6bd5be1e5db0ed5c0b57
Daniel Song <xdtsong@gmail.com>
2017-03-10 14:24:40 -0800

    Fix URL routing issues and add Login and Logout views for the Browsable API

--- manager/api/urls.py
+++ manager/api/urls.py
@@ -1,14 +1,20 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 
 # API Endpoints
 urlpatterns = [
-    url(r'^', views.api_root),
-    url(r'^albums/$', views.AlbumList, name='album-list'),
+    url(r'^$', views.api_root),
+    url(r'^albums/$', views.AlbumList.as_view(), name='album-list'),
     url(r'^albums/(?P<pk>[0-9]+)/$', views.AlbumDetail.as_view()),
-    url(r'^photos/$', views.PhotoList, name='photo-list'),
+    url(r'^photos/$', views.PhotoList.as_view(), name='photo-list'),
     url(r'^photos/(?P<pk>[0-9]+)/$', views.PhotoDetail.as_view()),
-    url(r'^users/$', views.UserList, name='user-list'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+]
+
+# Login and logout views for the browsable API
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
