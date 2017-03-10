ee3feb6f7f36a68ffc60437ea05e3ee4f2d081fe
Daniel Song <xdtsong@gmail.com>
2017-03-10 12:12:09 -0800

    Add __str__ methods for models and un-nest the resources in the serializers

--- manager/api/models.py
+++ manager/api/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Album(models.Model):
     title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 
 class Photo(models.Model):
     album = models.ForeignKey(Album, related_name='photos', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    photoUrl = models.URLField()
+    url = models.URLField()
     thumbnailUrl = models.URLField()
+
+    def __str__(self):
+        return self.title
--- manager/api/serializers.py
+++ manager/api/serializers.py
@@ -6,19 +6,11 @@ class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
-        fields = ('title', 'photoUrl', 'thumbnailUrl')
+        fields = ('id', 'album', 'title', 'url', 'thumbnailUrl')
 
 
 class AlbumSerializer(serializers.ModelSerializer):
-    photos = PhotoSerializer(many=True)
 
     class Meta:
         model = Album
-        fields = ('title', 'photos')
-
-    def create(self, validated_data):
-        photos_data = validated_data.pop('photos')
-        album = Album.objects.create(**validated_data)
-        for photo_data in photos_data:
-            Photo.objects.create(album=album, **photo_data)
-        return album
+        fields = ('id', 'title')
