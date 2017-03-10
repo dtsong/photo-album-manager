68d5ed369647138cedd9ba38ad0ddbfde2d2d915
Daniel Song <xdtsong@gmail.com>
2017-03-10 01:06:30 -0800

    Refactor serializers.py to use photo source

--- manager/api/serializers.py
+++ manager/api/serializers.py
@@ -3,21 +3,13 @@ from .models import Photo, Album
 
 
 class PhotoSerializer(serializers.ModelSerializer):
-    album = serializers.ReadOnlyField(source='album.id')
 
     class Meta:
         model = Photo
         fields = ('id', 'title', 'album', 'photoUrl', 'thumbnailUrl')
-        depth = 1
-
-
-class AlbumPrimaryKeyRelatedField(serializers.PrimaryKeyRelatedField):
-    def display_value(self, instance):
-        return 'Album: %s' % (instance.title)
-
 
 class AlbumSerializer(serializers.ModelSerializer):
-    photos = PhotoSerializer(many=True)
+    photos = PhotoSerializer(many=True, source='photo_set')
 
     class Meta:
         model = Album
@@ -29,20 +21,3 @@ class AlbumSerializer(serializers.ModelSerializer):
         for photo_data in photos_data:
             Photo.objects.create(album=album, **photo_data)
         return album
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data['title']
-        instance.save()
-
-        # Delete photos not included in the request
-        photo_ids = [item['id'] for item in validated_data['photos']]
-        for photo in instance.albums:
-            if photo.id not in photo_ids:
-                photo.delete()
-
-        # Create or update photo instances that are in the request
-        for photo in validated_data['photos']:
-            photo = Photo(id=photo['id'], title=photo['title'], album=instance,
-                          photoUrl=photo['photoUrl'], thumbnailUrl=photo['thumbnailUrl'])
-            photo.save()
-        return instance
