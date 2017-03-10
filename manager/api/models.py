ddf56b5b22fa00f87e9bf6c074f52f5503c2be4d
Daniel Song <xdtsong@gmail.com>
2017-03-10 14:03:23 -0800

    Add user foreign key to album and associate with Django User model

--- manager/api/models.py
+++ manager/api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Album(models.Model):
     title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, related_name='albums', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
